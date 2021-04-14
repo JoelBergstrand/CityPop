757ea58ea8dc7757bd4df942342c3b5eed6a742d
Joel Bergstrand <joel.bergstrand@hotmail.com>
2021-04-14 11:28:18 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * CityPop
+ */
+
+import React from 'react';
+import {Text, } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Hello World</Text>
+  );
+}
+
+
+export default App;
